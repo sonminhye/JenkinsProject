ba013c31afe2dc709b7071b32361a4bbf11f3896
sonminhye <smhgood3@gmail.com>
2021-09-16 23:06:21 +0900

    first commit

---
+++ src/main/java/com/test/jenkins/JenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+
+}
