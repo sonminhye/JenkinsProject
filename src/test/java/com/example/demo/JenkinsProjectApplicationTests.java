243ebf602a576b619a8d398d990de686daa81be8
sonminhye <smhgood3@gmail.com>
2021-09-27 22:51:34 +0900

    modify packaging jar to war

--- src/test/java/com/example/demo/JenkinsProjectApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsProjectApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.test.jenkins.JenkinsProjectApplication;
+
+@SpringBootTest(classes=JenkinsProjectApplication.class)
 class JenkinsProjectApplicationTests {
 
 	@Test
