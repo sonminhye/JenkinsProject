15aea41849c5fbf24d58b9bbf55043e60530ed07
sonminhye <smhgood3@gmail.com>
2021-09-28 22:44:57 +0900

    add main application extends SpringBootServletInitializer

--- src/main/java/com/test/jenkins/JenkinsProjectApplication.java
+++ src/main/java/com/test/jenkins/JenkinsProjectApplication.java
@@ -2,9 +2,10 @@ package com.test.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsProjectApplication {
+public class JenkinsProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
